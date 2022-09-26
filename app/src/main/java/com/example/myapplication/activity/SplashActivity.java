b38fd375bdc0bc44afe7a76df05a984da28f92eb
li <1650088191@qq.com>
2022-09-26 17:21:44 +0800

    启动页面

--- app/src/main/java/com/example/myapplication/activity/SplashActivity.java
+++ app/src/main/java/com/example/myapplication/activity/SplashActivity.java
@@ -2,7 +2,10 @@ package com.example.myapplication.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
 
 import com.example.myapplication.R;
 
@@ -10,7 +13,33 @@ public class SplashActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        //隐藏状态框
+        supportRequestWindowFeature(Window.FEATURE_NO_TITLE);
+        //全屏显示
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+        //停顿3秒
+        Thread myThread=new Thread() {
+            @Override
+            public void run() {
+                try {
+                    sleep(3000);
+                    //跳转到主页
+                    Intent intent=new Intent(SplashActivity.this,MainActivity.class);
+                    startActivity(intent);
+                    //关闭当前活动
+                    finish();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        };
+        //启动线程
+        myThread.start();
+
     }
+
+
 }
