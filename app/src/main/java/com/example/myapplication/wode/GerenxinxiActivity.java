10bcb7f21f124a2d2b7000f3c7be938ee62d8591
liuting0809 <liuting080910@163.com>
2022-09-26 21:02:41 +0800

    xxx

---
+++ app/src/main/java/com/example/myapplication/wode/GerenxinxiActivity.java
@@ -0,0 +1,16 @@
+package com.example.myapplication.wode;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.myapplication.R;
+
+public class GerenxinxiActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_gerenxinxi);
+    }
+}
