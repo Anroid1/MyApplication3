dc1350f1abb5a5d06535018fd9677899968f2768
leiganbuaideboluo <3233379092@qq.com>
2022-10-14 19:58:43 +0800

    完善的登录，注册，微信授权

--- app/src/main/java/com/example/myapplication/fragment/ShouyeFragment.java
+++ app/src/main/java/com/example/myapplication/fragment/ShouyeFragment.java
@@ -7,18 +7,23 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ViewFlipper;
 
 import com.example.myapplication.R;
 
 /**
  * shouye
  */
-public class ShouyeFragment extends Fragment {
+public class  ShouyeFragment extends Fragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_shouye, container, false);
+
+
     }
+
+
 }
