1e6f8255cd4ea8bdf61206357ebd64936a8e2ed6
li <1650088191@qq.com>
2022-10-19 21:23:21 +0800

    购物车提交

---
+++ app/src/main/java/com/example/myapplication/gouwuche/ShangpingjmActivity.java
@@ -0,0 +1,16 @@
+package com.example.myapplication.gouwuche;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.myapplication.R;
+
+public class ShangpingjmActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_shangpingjm);
+    }
+}
