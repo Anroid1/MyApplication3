2e6fa5ebc96f6a5d15d97a3b91da8d6686c13397
li <1650088191@qq.com>
2022-11-09 11:51:49 +0800

    提交

--- app/src/main/java/com/example/myapplication/activity/ShangpinActivity.java
+++ app/src/main/java/com/example/myapplication/activity/ShangpinActivity.java
@@ -11,26 +11,38 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.myapplication.R;
+import com.example.myapplication.adapter.CartAdapter;
 import com.example.myapplication.entity.Goods;
 import com.example.myapplication.util.CartDBService;
 
+import java.util.HashMap;
+
 public class ShangpinActivity extends AppCompatActivity {
     private int amount = 1;
-    int imgs[] = {R.drawable.lizi, R.drawable.juzi,R.drawable.xiyou,R.drawable.shouye3,
-            R.drawable.fanqie,R.drawable.caomei};
-    String goods[]={"丹东99草莓新鲜水果个大且甜现摘现在现发货应季水果包邮送到家 ...","爱媛38号果冻橙新鲜橙子当季水果水果柑橘蜜桔子整箱大果橙包邮 ... ",
-            "当季蔬菜套餐包括西兰花生菜包菜大西芹青瓜小南瓜等新鲜的蔬菜 ...","豌豆荚","番茄","丹东99草莓新鲜水果个大且甜现摘现在现发货应季水果包邮送到家 ..."};
+    private HashMap<Object,Integer> pichOnMap;//记录子项选择状态
 
-    String info[]={"清肺润心/家中必备","清肺润心/家中必备","清肺润心/家中必备","清肺润心/家中必备"
-            ,"清肺润心/家中必备","清肺润心/家中必备"};
-    String price[]={"￥13.20","￥5.00","￥4.00","￥4.50","￥2.00","￥3.30","￥5.20","￥5.00"};
-    String shuliang[]={"数量","数量","数量","数量","数量","数量"};
-    String fuwu[]={"规格","规格","规格","规格","规格","规格"};
-    int num[]={amount,amount,amount,amount,amount,amount};
-    String shijian[]={"中大果","60mm-70mm","新鲜蔬菜","中大果","中大果","中大果"};
-    String xiangqing[]={"商品详情","商品详情","商品详情","商品详情","商品详情","商品详情"};
-    int imgs1[] = {R.drawable.lizi, R.drawable.juzi,R.drawable.xiyou,R.drawable.shouye3,R.drawable.fanqie,R.drawable.caomei};
-    int imgs2[] = {R.drawable.lizi, R.drawable.juzi,R.drawable.xiyou,R.drawable.shouye3,R.drawable.fanqie,R.drawable.caomei};
+    public HashMap<Object, Integer> getPichOnMap() {
+        return pichOnMap;
+    }
+
+    public void setPichOnMap(HashMap<Object, Integer> pichOnMap) {
+        this.pichOnMap = pichOnMap;
+    }
+    int imgs[] = {R.drawable.lizi, R.drawable.juzi, R.drawable.xiyou, R.drawable.shouye3,
+            R.drawable.fanqie, R.drawable.caomei};
+    String goods[] = {"丹东99草莓新鲜水果个大且甜现摘现在现发货应季水果包邮送到家 ...", "爱媛38号果冻橙新鲜橙子当季水果水果柑橘蜜桔子整箱大果橙包邮 ... ",
+            "当季蔬菜套餐包括西兰花生菜包菜大西芹青瓜小南瓜等新鲜的蔬菜 ...", "豌豆荚", "番茄", "丹东99草莓新鲜水果个大且甜现摘现在现发货应季水果包邮送到家 ..."};
+
+    String info[] = {"清肺润心/家中必备", "清肺润心/家中必备", "清肺润心/家中必备", "清肺润心/家中必备"
+            , "清肺润心/家中必备", "清肺润心/家中必备"};
+    String price[] = {"￥13.20", "￥5.00", "￥4.00", "￥4.50", "￥2.00", "￥3.30", "￥5.20", "￥5.00"};
+    String shuliang[] = {"数量", "数量", "数量", "数量", "数量", "数量"};
+    String fuwu[] = {"规格", "规格", "规格", "规格", "规格", "规格"};
+    int num[] = {amount, amount, amount, amount, amount, amount};
+    String shijian[] = {"中大果", "60mm-70mm", "新鲜蔬菜", "中大果", "中大果", "中大果"};
+    String xiangqing[] = {"商品详情", "商品详情", "商品详情", "商品详情", "商品详情", "商品详情"};
+    int imgs1[] = {R.drawable.lizi, R.drawable.juzi, R.drawable.xiyou, R.drawable.shouye3, R.drawable.fanqie, R.drawable.caomei};
+    int imgs2[] = {R.drawable.lizi, R.drawable.juzi, R.drawable.xiyou, R.drawable.shouye3, R.drawable.fanqie, R.drawable.caomei};
 
 
     @Override
@@ -38,28 +50,28 @@ public class ShangpinActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_shangpin);
         //获取上一个页面传递的数据
-        Intent intent=getIntent();
-        int i=intent.getIntExtra("gid",0);
+        Intent intent = getIntent();
+        int i = intent.getIntExtra("gid", 0);
         //获取页面上的控件
-        ImageView iv_info=findViewById(R.id.iv_info);
-        TextView tv_info=findViewById(R.id.tv_info);
-        TextView tv_price=findViewById(R.id.tv_price);
-        TextView tv_number=findViewById(R.id.tv_number);
-        TextView tv_desc=findViewById(R.id.tv_desc);
-        TextView spt6=findViewById(R.id.spt6);
-        TextView spt7=findViewById(R.id.spt7);
-        TextView spt8=findViewById(R.id.spt8);
-        ImageView spI1=findViewById(R.id.spI1);
-        ImageView spI2=findViewById(R.id.spI2);
-        TextView tv_addcart=findViewById(R.id.tv_addcart);
-        TextView tv_buy=findViewById(R.id.tv_buy);
-        Button reduce=findViewById(R.id.btnDecrease);
-        Button add=findViewById(R.id.btnIncrease);
+        ImageView iv_info = findViewById(R.id.iv_info);
+        TextView tv_info = findViewById(R.id.tv_info);
+        TextView tv_price = findViewById(R.id.tv_price);
+        TextView tv_number = findViewById(R.id.tv_number);
+        TextView tv_desc = findViewById(R.id.tv_desc);
+        TextView spt6 = findViewById(R.id.spt6);
+        TextView spt7 = findViewById(R.id.spt7);
+        TextView spt8 = findViewById(R.id.spt8);
+        ImageView spI1 = findViewById(R.id.spI1);
+        ImageView spI2 = findViewById(R.id.spI2);
+        TextView tv_addcart = findViewById(R.id.tv_addcart);
+        TextView tv_buy = findViewById(R.id.tv_buy);
+        Button reduce = findViewById(R.id.btnDecrease);
+        Button add = findViewById(R.id.btnIncrease);
         //将数据添加到控件
         iv_info.setImageResource(imgs[i]);
         tv_info.setText(goods[i]);
         tv_price.setText(price[i]);
-        tv_number.setText(num[i]+"");
+        tv_number.setText(num[i] + "");
         tv_desc.setText(info[i]);
         spt6.setText(fuwu[i]);
         spt7.setText(shijian[i]);
@@ -68,50 +80,51 @@ public class ShangpinActivity extends AppCompatActivity {
         spI2.setImageResource(imgs2[i]);
         //加减控件实现
 
-            reduce.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    if (amount > 1)
-                        amount--;
-                    else
-                        Toast.makeText(ShangpinActivity.this, "已是最小数量", Toast.LENGTH_SHORT).show();
+        reduce.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (amount > 1)
+                    amount--;
+                else
+                    Toast.makeText(ShangpinActivity.this, "已是最小数量", Toast.LENGTH_SHORT).show();
 
-                    tv_number.setText(String.valueOf(amount));
+                tv_number.setText(String.valueOf(amount));
 
-                }
-            });
-            add.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    amount++;
-                    tv_number.setText(amount + "");
+            }
+        });
+        add.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                amount++;
+                tv_number.setText(amount + "");
 
-                }
+            }
 
-            });
+        });
         //加入购入车功能
         tv_addcart.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 String text = tv_number.getText().toString();
-                int w[] ={Integer.parseInt(text),Integer.parseInt(text),Integer.parseInt(text),Integer.parseInt(text)
-                        ,Integer.parseInt(text),Integer.parseInt(text)};
+                int w[] = {Integer.parseInt(text), Integer.parseInt(text), Integer.parseInt(text), Integer.parseInt(text)
+                        , Integer.parseInt(text), Integer.parseInt(text)};
                 //获取页面的数据
-                Goods good1=new Goods();
+                Goods good1 = new Goods();
                 good1.setName(goods[i]);
                 good1.setGuige(shijian[i]);
-                String s=price[i].substring(1);
+                String s = price[i].substring(1);
                 good1.setPrice((Float.parseFloat(s)));
                 good1.setNum(w[i]);
                 good1.setImg(imgs[i]);
                 //将数据存入SQLite数据表
-                CartDBService cartDBService=new CartDBService(ShangpinActivity.this);
+                CartDBService cartDBService = new CartDBService(ShangpinActivity.this);
                 cartDBService.addCart(good1);
-                Toast.makeText(ShangpinActivity.this,"商品已添加",Toast.LENGTH_SHORT).show();
+                Toast.makeText(ShangpinActivity.this, "商品已添加", Toast.LENGTH_SHORT).show();
 
             }
         });
 
     }
-
 }
+
+
--- app/src/main/java/com/example/myapplication/fragment/GouwucheFragment.java
+++ app/src/main/java/com/example/myapplication/fragment/GouwucheFragment.java
@@ -7,10 +7,14 @@ import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.SimpleAdapter;
 import android.widget.TextView;
@@ -21,18 +25,25 @@ import com.example.myapplication.activity.MainActivity;
 import com.example.myapplication.adapter.CartAdapter;
 import com.example.myapplication.entity.Goods;
 import com.example.myapplication.gouwuche.CgouwucheFragment;
+import com.example.myapplication.gouwuche.ComfireActivity;
 import com.example.myapplication.gouwuche.ShangpingjmActivity;
 import com.example.myapplication.util.CartDBService;
 
+import java.text.DecimalFormat;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 
 /**
  * 购物车
  */
-public class GouwucheFragment extends Fragment {
+public class GouwucheFragment extends Fragment implements CartAdapter.JiPriceInterface {
+    List<Goods> items;
+    private float totalCount;// 购买的商品总价
+    TextView zprice;
+    ImageView jisuan;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
@@ -41,13 +52,17 @@ public class GouwucheFragment extends Fragment {
         //从视图中获得控件
         ListView myList=view.findViewById(R.id.list1);
         TextView bianji=view.findViewById(R.id.bianji);
+        CheckBox b2=view.findViewById(R.id.mygouwuchecheckBox);
+        zprice=view.findViewById(R.id.zprice);
+        jisuan=view.findViewById(R.id.jiesuan);
         //查询数据库
         CartDBService cartDBService=new CartDBService(getContext());
         cartDBService.getAllCart();
-        List<Goods> items=cartDBService.getAllCart();
+        items=cartDBService.getAllCart();
         //实例化适配器
         CartAdapter cartAdapter=new CartAdapter(getContext(),items);
         myList.setAdapter(cartAdapter);
+        cartAdapter.setJiPriceInterface(this);
 
         //点击编辑标签跳转页面
         bianji.setOnClickListener(new View.OnClickListener() {
@@ -64,6 +79,75 @@ public class GouwucheFragment extends Fragment {
                 .commit();
             }
         });
+        //实现全选
+        b2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                HashMap<Object, Integer> map = cartAdapter.getPichOnMap();
+                boolean isCheck = false;
+                boolean isUnCheck = false;
+                Iterator iterator = map.entrySet().iterator();
+                while (iterator.hasNext()) {
+                    Map.Entry entry = (Map.Entry) iterator.next();
+                    if (Integer.valueOf(entry.getValue().toString()) == 1) {
+                        isCheck = true;
+                    } else {
+                        isUnCheck = true;
+                    }
+                }
+                if (isCheck == true && isUnCheck == false) {
+                    //已经全选，做反选
+                    for (int i = 0; i < items.size(); i++) {
+                        map.put(items.get(i).getId(), 0);
+                    }
+                    b2.setChecked(false);
+                    //做全选
+                } else if (isCheck == false && isUnCheck == true) {
+                    for (int i = 0; i < items.size(); i++) {
+                        map.put(items.get(i).getId(), 1);
+                    }
+                    b2.setChecked(true);
+
+                } else if (isCheck == true && isUnCheck == true) {
+                    //部分选中
+                    for (int i = 0; i < items.size(); i++) {
+                        map.put(items.get(i).getId(), 1);
+
+                    }
+                    b2.setChecked(true);
+                }
+                priceControl(map);
+                cartAdapter.setPichOnMap(map);
+                cartAdapter.notifyDataSetChanged();
+            }
+        });
+        jisuan.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (totalCount<=0){
+                    Toast.makeText(getActivity(),"请选择需要付款的商品",Toast.LENGTH_LONG).show();
+
+                }else{
+                    Intent intent=new Intent(getActivity(), ComfireActivity.class);
+                    //被选中的项目
+                    HashMap<Object,Integer> map=cartAdapter.getPichOnMap();
+                    int id[]=new int[items.size()];
+                    int a=0;
+                    for (int i=0;i<items.size();i++){
+
+                        if (map.get(items.get(i).getId())==1){
+                            intent.putExtra(i+"",items.get(i));
+                            id[a]=i;
+                            a++;
+                        }
+                    }
+                    intent.putExtra("id",id);
+                    startActivity(intent);
+                }
+            }
+        });
+
+
         //list跳转商品详情页
         myList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
@@ -76,4 +160,21 @@ public class GouwucheFragment extends Fragment {
         return view;
     }
 
+    @Override
+    public void jiPrice(HashMap<Object, Integer> pitchOnMap) {
+        priceControl(pitchOnMap);
+    }
+    public void  priceControl(HashMap<Object,Integer> pitchOnMap){
+        totalCount=0;
+         //控制总价的显示
+        for (int i=0;i< items.size();i++){
+        if (pitchOnMap.get(items.get(i).getId())==1){
+        totalCount=totalCount+items.get(i).getPrice()*items.get(i).getNum();
+        }
+        }
+        DecimalFormat decimalFormat=new DecimalFormat("0.00");
+        zprice.setText(decimalFormat.format(totalCount));
+
+
+        }
 }
