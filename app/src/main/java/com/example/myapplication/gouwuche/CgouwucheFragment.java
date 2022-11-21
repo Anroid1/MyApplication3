88c83922f6b3c36d7ba0bd96ecb68b8a0e1656c4
li <1650088191@qq.com>
2022-11-21 11:28:35 +0800

    提交

--- app/src/main/java/com/example/myapplication/gouwuche/CgouwucheFragment.java
+++ app/src/main/java/com/example/myapplication/gouwuche/CgouwucheFragment.java
@@ -3,13 +3,16 @@ package com.example.myapplication.gouwuche;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.CheckBox;
 import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.SimpleAdapter;
+import android.widget.Toast;
 
 import com.example.myapplication.R;
 import com.example.myapplication.adapter.CartAdapter;
@@ -19,31 +22,100 @@ import com.example.myapplication.util.CartDBService;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 
-public class CgouwucheFragment extends Fragment {
+public class CgouwucheFragment extends Fragment implements CartAdapter.JiPriceInterface{
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        View view=inflater.inflate(R.layout.fragment_cgouwuche, container, false);
+        View view = inflater.inflate(R.layout.fragment_cgouwuche, container, false);
         //从视图中获得控件
-        ListView myList=view.findViewById(R.id.list1);
-        ImageView qubian=view.findViewById(R.id.qubian);
+        ListView myList = view.findViewById(R.id.list1);
+        ImageView qubian = view.findViewById(R.id.qubian);
+        CheckBox b2 = view.findViewById(R.id.mygouwuchecheckBox);
+        ImageView shangchu=view.findViewById(R.id.shangchu);
+
+
         //查询数据库
-        CartDBService cartDBService=new CartDBService(getContext());
+        CartDBService cartDBService = new CartDBService(getContext());
         cartDBService.getAllCart();
-        List<Goods> items=cartDBService.getAllCart();
+        List<Goods> items = cartDBService.getAllCart();
         //实例化适配器
-        CartAdapter cartAdapter=new CartAdapter(getContext(),items);
+        CartAdapter cartAdapter = new CartAdapter(getContext(), items);
         myList.setAdapter(cartAdapter);
+        cartAdapter.setJiPriceInterface(this);
+        //实现全选
+        b2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                HashMap<Object, Integer> map = cartAdapter.getPichOnMap();
+                boolean isCheck = false;
+                boolean isUnCheck = false;
+                Iterator iterator = map.entrySet().iterator();
+                while (iterator.hasNext()) {
+                    Map.Entry entry = (Map.Entry) iterator.next();
+                    if (Integer.valueOf(entry.getValue().toString()) == 1) {
+                        isCheck = true;
+                    } else {
+                        isUnCheck = true;
+                    }
+                }
+                if (isCheck == true && isUnCheck == false) {
+                    //已经全选，做反选
+                    for (int i = 0; i < items.size(); i++) {
+                        map.put(items.get(i).getId(), 0);
+                    }
+                    b2.setChecked(false);
+                    //做全选
+                } else if (isCheck == false && isUnCheck == true) {
+                    for (int i = 0; i < items.size(); i++) {
+                        map.put(items.get(i).getId(), 1);
+                    }
+                    b2.setChecked(true);
+
+                } else if (isCheck == true && isUnCheck == true) {
+                    //部分选中
+                    for (int i = 0; i < items.size(); i++) {
+                        map.put(items.get(i).getId(), 1);
+
+                    }
+                    b2.setChecked(true);
+                }
+                cartAdapter.setPichOnMap(map);
+                cartAdapter.notifyDataSetChanged();
+            }
+        });
+        //实现删除
+        shangchu.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //被选中的
+                HashMap<Object,Integer>map=cartAdapter.getPichOnMap();
+                int[] id=new int[items.size()];
+                for (int i=0;i< items.size();i++){
+                    if (map.get(items.get(i).getId())==1){
+                        int id1=items.get(i).getId();
+                        cartDBService.deleteCart(id1);
+                        Toast.makeText(getActivity(),"商品删除成功",Toast.LENGTH_SHORT).show();
+
+                    }
+                }
+                cartAdapter.notifyDataSetChanged();
+
+
+
+            }
+        });
         qubian.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 //相应控件的监听里面实现跳转
-                FragmentManager fragmentManager=getFragmentManager();
+                FragmentManager fragmentManager = getFragmentManager();
                 //开启事务
                 fragmentManager.beginTransaction()
                         //替换frament
@@ -55,5 +127,10 @@ public class CgouwucheFragment extends Fragment {
             }
         });
         return view;
+    }
+
+    @Override
+    public void jiPrice(HashMap<Object, Integer> pitchOnMap) {
+
     }
 }
--- app/src/main/java/com/example/myapplication/gouwuche/ComfireActivity.java
+++ app/src/main/java/com/example/myapplication/gouwuche/ComfireActivity.java
@@ -2,34 +2,141 @@ package com.example.myapplication.gouwuche;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
-import android.view.MotionEvent;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
 import android.widget.ListView;
+import android.widget.ScrollView;
+import android.widget.SimpleAdapter;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.myapplication.R;
+import com.example.myapplication.adapter.CartAdapter;
+import com.example.myapplication.entity.Goods;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+
 
 public class ComfireActivity extends AppCompatActivity {
-    ListView list1;
-
-    @SuppressLint("ClickableViewAccessibility")
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    ListView list2;
+    TextView tv_count;
+    Button tv_comfire;
+    CheckBox radio1,radio2,radio3;
+    float totalprice=0;
+    protected void onCreate (Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_comfire);
-        list1=findViewById(R.id.list1);
-        list1.setOnTouchListener(new View.OnTouchListener() {
+            list2 = findViewById(R.id.list2);
+            tv_count=findViewById(R.id.tv_acount);
+            tv_comfire=findViewById(R.id.tv_comfire);
+            radio1=findViewById(R.id.radio1);
+            radio2=findViewById(R.id.radio2);
+            radio3=findViewById(R.id.radio3);
+            initView();
 
-            @Override
-            public boolean onTouch(View view, MotionEvent event) {
-                switch (event.getAction()) {
-                    case MotionEvent.ACTION_MOVE:
-                        return true;
-                    default:
-                        break;
+
+            tv_comfire.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+
                 }
-                return true;
+            });
+
+
+            //获取Intent数据，初始化页面
+            Intent intent = getIntent();
+            int a = intent.getIntExtra("id", 0);
+            ArrayList<Goods> goodsList = new ArrayList<Goods>();
+            for (int i = 0
+                 ; i < a; i++) {
+                Goods goods = new Goods();
+                goods = (Goods) intent.getSerializableExtra(i + "");
+                goodsList.add(goods);
+            }
+            //封装数据
+            List<Map<String, Object>> lists = new ArrayList<Map<String, Object>>();
+            for (int i = 0; i < goodsList.size(); i++) {
+                Goods goods = goodsList.get(i);
+                Map<String, Object> item = new HashMap<String, Object>();
+                item.put("name", goods.getName());
+                item.put("price", goods.getPrice());
+                item.put("num", goods.getNum());
+                item.put("img", goods.getImg());
+                item.put("guige", goods.getGuige());
+                totalprice=totalprice+goods.getPrice()* goods.getNum();
+                lists.add(item);
+                tv_count.setText(totalprice+6+"");
+
+            }
+
+            SimpleAdapter simpleAdapter = new SimpleAdapter(this, lists, R.layout.comfire_items, new String[]{"name", "price", "num", "img", "guige"},
+                    new int[]{R.id.tv_name, R.id.jiage, R.id.tv_shuliang, R.id.iv_img, R.id.tv_desc});
+            list2.setAdapter(simpleAdapter);
+            //处理方案：
+            int totalHeight = 0;
+            // 如果没有设置数据适配器，则ListView没有子项，返回。
+            for (int index = 0, len = simpleAdapter.getCount(); index < len; index++) {
+                View listViewItem = simpleAdapter.getView(index, null, list2);
+                // 计算子项View 的宽高
+                listViewItem.measure(0, 0);
+                // 计算所有子项的高度和
+                totalHeight += listViewItem.getMeasuredHeight();
+            }
+            ViewGroup.LayoutParams params = list2.getLayoutParams();
+            // listView.getDividerHeight()获取子项间分隔符的高度
+            // params.height设置ListView完全显示需要的高度
+            params.height = totalHeight + (list2.getDividerHeight() * (simpleAdapter.getCount() - 1));
+            list2.setLayoutParams(params);
+            ScrollView scrollview = (ScrollView) findViewById(R.id.ll_info);
+            scrollview.smoothScrollTo(0, 0);
+
+        }
+
+    private void initView() {
+        //监听选中取消事件
+        radio1.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (isChecked){
+                    radio1.setChecked(true);
+                    radio2.setChecked(false);
+                    radio3.setChecked(false);
+                }else {
+                    radio1.setChecked(false);
+                }
+            }
+        });
+        radio2.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (isChecked){
+                    radio1.setChecked(false);
+                    radio2.setChecked(true);
+                    radio3.setChecked(false);
+                }else {
+                    radio2.setChecked(false);
+                }
+            }
+        });
+        radio3.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (isChecked){
+                    radio1.setChecked(false);
+                    radio3.setChecked(true);
+                    radio2.setChecked(false);
+                }else {
+                    radio3.setChecked(false);
+                }
             }
         });
     }
